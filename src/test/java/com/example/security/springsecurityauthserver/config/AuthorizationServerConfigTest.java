7823344e6f44e5ea934c5f67c29b8e1245aaac09
PradeepNegi <pradeepnegi2012@gmail.com>
2020-01-14 17:46:16 +0530

    Updated the tests for Autherization Server.

--- src/test/java/com/example/security/springsecurityauthserver/config/AuthorizationServerConfigTest.java
+++ src/test/java/com/example/security/springsecurityauthserver/config/AuthorizationServerConfigTest.java
@@ -11,7 +11,7 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -20,19 +20,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class AuthorizationServerConfigTest {
     @Autowired
     private MockMvc mockMvc;
+    String urlTemplate = "/oauth/token";
 
     @Test
     void itShouldAuthenticate() throws Exception {
-        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("grant_type", "password");
-        params.add("username", "admin2");
-        params.add("password", "admin2");
+        MultiValueMap<String, String> params = getStringStringMultiValueMap();
 
         String base64ClientCredentials = new String(Base64.encodeBase64("12345:12345".getBytes()));
-
-
         ResultActions result
-                = mockMvc.perform(post("/oauth/token")
+                = mockMvc.perform(post(urlTemplate)
                 .params(params)
                 .header("Authorization", "Basic " + base64ClientCredentials)
                 .accept("application/json;charset=UTF-8"))
@@ -43,4 +39,24 @@ class AuthorizationServerConfigTest {
         JacksonJsonParser jsonParser = new JacksonJsonParser();
         assertNotNull(jsonParser.parseMap(resultString).get("access_token").toString());
     }
+
+    @Test
+    void itShouldUnAuthenticate() throws Exception {
+        String base64ClientCredentials = new String(Base64.encodeBase64("23e23e:23434".getBytes()));
+        mockMvc.perform(post(urlTemplate)
+                .params(getStringStringMultiValueMap())
+                .header("Authorization", "Basic " + base64ClientCredentials)
+                .accept("application/json;charset=UTF-8"))
+                .andExpect(status().isUnauthorized());
+
+    }
+
+    private MultiValueMap<String, String> getStringStringMultiValueMap() {
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "password");
+        params.add("username", "admin2");
+        params.add("password", "admin2");
+        return params;
+    }
+
 }
